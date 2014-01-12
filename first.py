0187cd5f65933fd53c15229e39602b8b4276e177
Jose Luiz <jlarantes@gmail.com>
2014-01-12 13:37:06 -0200

    test

---
+++ first.py
@@ -0,0 +1,8 @@
+from urllib import urlopen
+with urlopen('http://sixty-north.com/c/t.txt') as story:
+  story_words = []
+  for line in story:
+    line_words = line.decode('utlf-8').split()
+    for word in line_words:
+      story_words.append(word)
+
